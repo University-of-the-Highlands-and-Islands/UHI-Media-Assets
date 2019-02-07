ac76a1aa9ae2bf7c34aa02a8db168e229e8e657b
Paul Walker <paul.walker@uhi.ac.uk>
2019-02-07 11:33:55 +0000

    last modified function added
    Called from One Web - Left Nav Level 3 to display the last modified dates on recently added pages.  Restricted to the Help site for now.

--- University/Admin assets/JS/source/main.js
+++ University/Admin assets/JS/source/main.js
@@ -255,6 +255,22 @@ function uhiCollapseSection(open){
   }
 }
 
+function lastModified(slastmodified,rootUrl){
+	if(rootUrl.includes("t4help") || rootUrl.includes("dev-www.t4.uhi.ac.uk")){
+    var dlastmodified = new Date(slastmodified);
+    var cutOff = new Date().setDate(new Date().getDate()-60);
+    var markup = '<div class="meta--last-updated">This page was last updated <time datetime=' + $.datepicker.formatDate("yy-MM-dd+00:00", new Date(dlastmodified)) + '>' + $.datepicker.formatDate("d MM yy", new Date(dlastmodified)) + '</time></div>';   
+
+    if(dlastmodified < cutOff)
+    {
+      document.write("<!--" + markup + "-->");
+    }
+  else
+    {
+      document.write(markup);
+    }
+  }
+}
 
 
 
